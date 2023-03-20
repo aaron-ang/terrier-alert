ad92d01579b7ca8f544cbfb06c8e582ffcffb00d
aaron-ang <67321817+aaron-ang@users.noreply.github.com>
2023-03-20 00:08:55 -0400

    include schema, update db headers

--- db.py
+++ db.py
@@ -10,22 +10,22 @@ course_collection = mongo_db["courses"]
 user_collection = mongo_db["users"]
 
 
-def find_all_courses():
+def get_all_courses():
     """Find all active courses in database and return iterable of collection objects"""
     return course_collection.find()
 
 
-def find_all_users():
+def get_all_users():
     """Find all users in database and return iterable of collection objects"""
     return user_collection.find()
 
 
-def find_user(uid: str):
+def get_user(uid: str):
     """Find user in database and return collection object (dict)"""
     return user_collection.find_one({"user": uid})
 
 
-def find_user_course(uid: str):
+def get_user_course(uid: str):
     """Find course which user is subscribed to and return the collection object (dict)"""
     return course_collection.find_one({"users": uid})
 
@@ -35,24 +35,24 @@ def remove_course(course: str):
     course_collection.delete_one({"name": course})
 
 
-def remove_user(course: str, uid: str):
+def unsubscribe(course: str, uid: str):
     """Remove user from database"""
     course_collection.update_one({"name": course}, {"$pull": {"users": uid}})
 
 
-def update_user_subscription_time(uid: str, time: datetime):
+def update_subscription_time(uid: str, time: datetime):
     """Update user's most recent subscription time"""
     user_collection.update_one(
         {"user": uid}, {"$set": {"last_subscribed": time}}, upsert=True)
 
 
-def update_user_subscription_status(uid: str, is_subscribed: bool):
+def update_subscription_status(uid: str, is_subscribed: bool):
     """Update user's most recent unsubscription status"""
     user_collection.update_one(
         {"user": uid}, {"$set": {"is_subscribed": is_subscribed}})
 
 
-def update_db(course_name: str, uid: str):
+def subscribe(course_name: str, uid: str):
     """Update course with new user, inserting new course if necessary"""
     course_collection.update_one(
         {"name": course_name}, {"$push": {"users": uid}}, upsert=True)
