284623bd10d40683819c3b996c105a31ea18e60f
aaron-ang <67321817+aaron-ang@users.noreply.github.com>
2023-04-16 10:43:12 -0400

    add semester field to `courses` collection

--- db.py
+++ db.py
@@ -2,6 +2,8 @@ import os
 from datetime import datetime
 from pymongo import MongoClient
 
+from course import Course
+
 MONGO_URL = str(os.getenv("MONGO_URL"))
 
 mongo_client = MongoClient(MONGO_URL)
@@ -15,6 +17,31 @@ def get_all_courses():
     return course_collection.find()
 
 
+def get_user_course(uid: str):
+    """Find course which user is subscribed to and return the collection object (dict)"""
+    return course_collection.find_one({"users": uid})
+
+
+def subscribe(course_name: str, uid: str):
+    """Update course with new user, inserting new course if necessary"""
+    semester = f"{Course.SEMESTER} {Course.YEAR}"
+    course_collection.update_one({"name": course_name, "semester": semester},
+                                 {"$setOnInsert": {"semester": semester},
+                                  "$push": {"users": uid}},
+                                 upsert=True)
+
+
+def unsubscribe(course: str, uid: str):
+    """Remove user from course"""
+    course_collection.update_one({"name": course},
+                                 {"$pull": {"users": uid}})
+
+
+def remove_course(course: str):
+    """Remove course from database"""
+    course_collection.delete_one({"name": course})
+
+
 def get_all_subscribed_users():
     """Find all users in database and return iterable of collection objects"""
     return user_collection.find({"is_subscribed": True})
@@ -25,34 +52,15 @@ def get_user(uid: str):
     return user_collection.find_one({"user": uid})
 
 
-def get_user_course(uid: str):
-    """Find course which user is subscribed to and return the collection object (dict)"""
-    return course_collection.find_one({"users": uid})
-
-
-def remove_course(course: str):
-    """Remove course from database"""
-    course_collection.delete_one({"name": course})
-
-
-def unsubscribe(course: str, uid: str):
-    """Remove user from database"""
-    course_collection.update_one({"name": course}, {"$pull": {"users": uid}})
-
-
 def update_subscription_time(uid: str, time: datetime):
     """Update user's most recent subscription time"""
-    user_collection.update_one(
-        {"user": uid}, {"$set": {"last_subscribed": time}}, upsert=True)
+    user_collection.update_one({"user": uid},
+                               {"$set": {"last_subscribed": time}},
+                               upsert=True)
 
 
 def update_subscription_status(uid: str, is_subscribed: bool):
     """Update user's most recent unsubscription status"""
-    user_collection.update_one(
-        {"user": uid}, {"$set": {"is_subscribed": is_subscribed}}, upsert=True)
-
-
-def subscribe(course_name: str, uid: str):
-    """Update course with new user, inserting new course if necessary"""
-    course_collection.update_one(
-        {"name": course_name}, {"$push": {"users": uid}}, upsert=True)
+    user_collection.update_one({"user": uid},
+                               {"$set": {"is_subscribed": is_subscribed}},
+                               upsert=True)
